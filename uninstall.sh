ccd7f98bb0aeecfeeaaf3639645d83ece8f56f11
Frak <frak.al-nuaimy@siemens.com>
2024-06-07 10:43:49 -0400

    🎉 Initial Commit

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf "/Library/Application Support/Paragon Software/"
+rm -rf /Library/LaunchDaemons/com.paragon-software.extfs.loader.plist
+rm -rf /Library/LaunchDaemons/com.paragon-software.extfsd.plist
+rm -rf /Library/LaunchAgents/com.paragon-software.extfs.notification-agent.plist
+rm -rf /Library/Filesystems/ufsd_ExtFS.fs/
+rm -rf /Library/Extensions/ufsd_ExtFS.kext/
+rm -rf /Library/PreferencePanes/ParagonExtFS.prefPane/
+rm -rf /Users/frak/Library/Preferences/com.paragon-software.extfs.fsapp.plist
+rm -rf /Applications/extFS for Mac.app
